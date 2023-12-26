f360f1e37821c3bd3b9ffcd9bfdfd762742b9d73
Harsh <harshdivate45@gmail.com>
2023-12-26 14:15:40 +0530

    registered user

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,6 +1,33 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import connectDB from "../config/db.js";
 import bcrypt from "bcrypt";
+import { uploadFiletoCloudinary } from "../utils/cloudinary.js";
+import jwt from "jsonwebtoken";
+
+const generateRefreshAccessToken = (username, email, password) => {
+  const accessToken = jwt.sign(
+    {
+      username,
+      email,
+      password,
+    },
+    process.env.ACCESS_TOkEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOkEN_EXPIRY_DATE,
+    }
+  );
+
+  const refreshToken = jwt.sign(
+    {
+      email,
+    },
+    process.env.ACCESS_TOkEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOkEN_EXPIRY_DATE,
+    }
+  );
+  return { accessToken, refreshToken };
+};
 
 const registerUser = asyncHandler(async (req, res) => {
   // To-do register user
@@ -12,34 +39,66 @@ const registerUser = asyncHandler(async (req, res) => {
   // send response
 
   try {
+    const { username, email, password } = req.body;
+
     console.log(req.body);
-    return res.status(200).json({});
-    // const { username, email, password, image } = req.body;
-    // console.log(req.body);
-    // if ([username, email, password, image].some((val) => val?.trim() === "")) {
-    //   return res
-    //     .status(400)
-    //     .json({ status: "false", message: "Username or email undefined" });
-    // }
-    // const client = await connectDB();
-    // const userExist = await client.query(
-    //   `select * from users where email='${email}' or username='${username}'`
-    // );
-    // if (userExist.rows.length > 0) {
-    //   return res
-    //     .status(409)
-    //     .json({ status: "false", message: "user alreay exist" });
-    // }
-    // const hashedPassword = await bcrypt.hash(password, 10);
-    // console.log(hashedPassword);
-    // const query = `INSERT INTO user (username, email, password, image, accessToken, refreshToken)
-    //   VALUES
-    //   (${username}, ${email}, ${hashedPassword}, ${image},'123','13');`;
-    // const registerUser = await client.query(query);
-    // console.log(registerUser);
-    // return res
-    //   .status(200)
-    //   .json({ status: true, message: registerUser.rows[0] });
+
+    if ([username, email, password].some((val) => val?.trim() === "")) {
+      return res
+        .status(400)
+        .json({ status: "false", message: "Username or email undefined" });
+    }
+
+    const client = await connectDB();
+    const userExist = await client.query(
+      `select * from users where email='${email}' or username='${username}'`
+    );
+
+    if (userExist.rows.length > 0) {
+      return res
+        .status(409)
+        .json({ status: "false", message: "user alreay exist" });
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    if (!avatarLocalPath) {
+      return res
+        .status(400)
+        .json({ status: false, message: "Avatar file required" });
+    }
+
+    const avatar = await uploadFiletoCloudinary(avatarLocalPath);
+    if (!avatar) {
+      return res
+        .status(400)
+        .json({ status: false, message: "Avatar file required" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log(hashedPassword);
+
+    // Generate refreshToken and AccessToken
+    const { accessToken, refreshToken } = generateRefreshAccessToken(
+      username,
+      email,
+      password
+    );
+    const query = `INSERT INTO users (username, email, password, image, accessToken, refreshToken)
+      VALUES
+      ($1,$2,$3,$4,$5,$6) RETURNING username,email,image,accessToken,refreshToken;`;
+
+    const registerUser = await client.query(query, [
+      username,
+      email,
+      hashedPassword,
+      avatar.url,
+      accessToken,
+      refreshToken,
+    ]);
+
+    return res
+      .status(200)
+      .json({ status: true, message: registerUser.rows[0] });
   } catch (error) {
     console.log(error?.message);
     // client.release();
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+const uploadFiletoCloudinary = async (localFilePath) => {
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET_KEY,
+  });
+  try {
+    if (!localFilePath) {
+      return null;
+    }
+
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // fs.unlinkSync(localFilePath);
+
+    return response;
+  } catch (error) {
+    console.log("Error in file uploading");
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+
+export { uploadFiletoCloudinary };

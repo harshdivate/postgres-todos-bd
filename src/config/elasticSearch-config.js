1b37653878fbb62c7446ccceac53585850ba66b6
Harsh <harshdivate45@gmail.com>
2024-02-08 23:12:56 +0530

    insert todo controller

--- src/config/elasticSearch-config.js
+++ src/config/elasticSearch-config.js
@@ -2,17 +2,23 @@ import { Client, HttpConnection } from "@elastic/elasticsearch";
 import fs from "fs";
 
 export default async function configureElasticSearch() {
-  const client = await new Client({
-    node: "https://localhost:9200",
-    auth: {
-      username: "elastic",
-      password: "0MK64fntZwyD9tJhgUdO",
-    },
-    log: "trace",
-    tls: {
-      ca: fs.readFileSync("./public/http_ca.crt"),
-      rejectUnauthorized: false,
-    },
-  });
-  return client;
+  try {
+    const client = await new Client({
+      node: "https://localhost:9200", // make env variable
+      auth: {
+        username: "elastic", // env
+        password: "0MK64fntZwyD9tJhgUdO", // env
+      },
+      log: "trace",
+      tls: {
+        ca: fs.readFileSync("./public/http_ca.crt"),
+        rejectUnauthorized: false,
+      },
+    });
+    if (client) {
+      return client;
+    }
+  } catch (error) {
+    console.log("Error in connecting db" + error);
+  }
 }
